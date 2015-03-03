2a1d54cb97daa1f4a46855571d34619b6d30693d
Thomas Leskin <tleskin@gmail.com>
2015-03-03 16:59:27 -0700

    Calculated offsets from date

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,12 +1,41 @@
 class Offset
 
-  attr_reader :date
-
-  def initialize(date)
-    @date = date
+  def date
+    time = Time.now
+    time = time.strftime("%m%d%y")
   end
 
-  def rotation_a
-    "03"
+  def square(date)
+    if date.start_with?("0")
+      time = date.slice(1, 5).to_i
+    else
+      time = time.to_i
+    end
+    time * time
   end
+
+  def rotation_a(square)
+    square[0,1]
+  end
+
+  def rotation_b(square)
+    square[1,1]
+  end
+
+  def rotation_c(square)
+    square[2,1]
+  end
+
+  def rotation_d(square)
+    square[3,1]
+  end
+
+  #take the date
+  #square it
+  #grab the 4th from the end for the a offset 9
+  #grab the 3th from the end for the b offset 2
+  #grab the 2th from the end for the c offset 2
+  #grab the last number as the d offset       5
+  #918999225
+
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,13 +10,33 @@ class OffsetTest < Minitest::Test
   end
 
   def test_it_takes_in_a_date
-    offset = Offset.new("030315")
+    offset = Offset.new
     assert_equal "030315", offset.date
   end
 
-  def test_it_gets_rotation_A
-    offset = Offset.new("030315")
-    assert_equal "03", offset.rotation_a
+  def test_it_squares_the_date
+    offset = Offset.new
+    assert_equal 918999225, offset.square("030315")
+  end
+
+  def test_it_gets_rotation_a
+    offset = Offset.new
+    assert_equal "9", offset.rotation_a("9225")
+  end
+
+  def test_it_gets_rotation_b
+    offset = Offset.new
+    assert_equal "2", offset.rotation_b("9225")
+  end
+
+  def test_it_gets_rotation_c
+    offset = Offset.new
+    assert_equal "3", offset.rotation_c("9235")
+  end
+
+  def test_it_gets_rotation_d
+    offset = Offset.new
+    assert_equal "5", offset.rotation_d("9225")
   end
 
 end
